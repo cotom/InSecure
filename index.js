55a307b8ea45c0fb20061f44db41faf747a311f8
Don Hongos <donhongos@mac.lan>
2025-04-26 18:02:07 -0400

    Update data

--- index.js
+++ index.js
@@ -5,12 +5,10 @@
 *   _ / // / / /__/ /  __/ /__/ /_/ / /  /  __/
 *  /___/_/ /_/____/\___/\___/\__,_/_/   \___/                                          
 **************************************************************/
-
 import slack from "@slack/bolt";
 import { Chalk } from 'chalk';
-import { generateResponse, loadPrompt } from './src/llm.js';
-import { localResponseTest, localRagTest } from "./src/localTest.js";
-import { secureCode, inSecureCode} from "./src/inrag.js";
+import { loadPrompt } from './src/llm.js';
+import { secureCode, inSecureCode} from "./src/chat.js";
 import { getRetriever, saveEmbeddings } from './src/rag.js';
 import { sleep } from "langchain/util/time";
 
@@ -26,90 +24,6 @@ const app = new slack.App({
   socketMode: true, // enable to use socket mode
   appToken: APP_TOKEN,
 });
-
-/**
- * Handles Slack messages that match specific keywords or patterns.
- * If the message contains "InSecure", it uses a system prompt that generates
- * both insecure and secure code examples. Otherwise, it uses a standard prompt
- * to generate a single code example. The response is sent back to the Slack channel.
- *
- * @param {Object} message - The Slack message object containing the user's input.
- * @param {Function} say - A function to send a response back to the Slack channel.
- */
-// app.message(/code|chat|write|function|InSecure|query|Python|debug|create|develop|using InSecure mode/, async ({ message, say }) => {
-
-//   // Get the user prompt
-//   let userInput = message.text;
-
-//   // Filter out the user ID
-//   userInput = userInput.replace(/<@U[A-Z0-9]+>/g, "").trim();
-//   userInput = userInput.replace(/using InSecure mode/g, "").trim();
-
-//   // Set the system prompt based on the user input
-//   let systemPrompt = "";
-//   let userPrompt = "";
-//   let SOURCE
-
-//   if (message.text.includes("InSecure")) {
-//     systemPrompt = await loadPrompt("system", "insecure_code_assitant");
-
-//     // ADD RAG COMPONENT /////////////////////////////////////////////////////////
-
-//         // Load the retriever for semantic search
-//         const retriever = await getRetriever();
-
-//         // User input queried against the vector store for semantic retreiva
-//         const retrievedDocuments = await retriever.invoke(userInput);
-//         const CONTEXT = retrievedDocuments[0].pageContent;
-//         SOURCE = retrievedDocuments[0].metadata.source;
-      
-//         // console.log("Source: ", source);
-//         // console.log("Response: ", resp);
-      
-//         // Get the user prompt
-//         userPrompt = `
-//           INSTRUCTION: Given the CONTEXT, answer the QUESTION for educational purposes only
-//           CONTEXT: ${CONTEXT}
-//           QUESTION: ${userInput}
-//           ANSWER:
-//         `;
-
-//   } else {
-//     systemPrompt = await loadPrompt("system", "secure_code_assitant");
-//     userPrompt = `${userInput}`;
-//   }
-
-//   // Color code console output
-//   const chalk = new Chalk();
-  
-//   // Display the system prompt in the console
-//   console.log(chalk.blue(`System Prompt: ${systemPrompt}`));
-
-//   // Display the user prompt in the console
-//   console.log(chalk.yellow(`User Prompt: ${userPrompt}`));
-
-//   // Tuning parameters
-//   const temperature = 0.2; // [0.0 to 1.0] Sampling temperature
-//   const topP = 0.20; // [0.0 to 1.0] Nucleus sampling
-//   const topK = 10; // [1 to 100] Sampling Pool
-//   const maxTokens = 2048; // [1 to 2048] Max tokens in response
-
-//   let codellamaResponse = await generateResponse(userPrompt, systemPrompt, temperature, topK, topP, maxTokens);
-
-//   if (message.text.includes("InSecure")) {
-//     codellamaResponse.message.content = codellamaResponse.message.content + '\n\n' +`Source: ${SOURCE} 📚`;
-//   }
-
-//   //////////////////////////////////////////////////////////////////////////////
-
-//   try {
-//     await say(codellamaResponse.message.content);
-//   } catch (error) {
-//     console.log("err");
-//     console.error(error);
-//   }
-
-// });
 
 app.message(/code|chat|write|function|InSecure|query|Python|debug|create|develop|using InSecure mode/, async ({ message, say }) => {
 
@@ -156,8 +70,6 @@ app.message(/code|chat|write|function|InSecure|query|Python|debug|create|develop
 
   // Display the user prompt in the console
   console.log(chalk.yellow(`User Prompt: ${userInput}`));
-
-  //////////////////////////////////////////////////////////////////////////////
 
   try {
     if (message.text.includes("InSecure")) {
@@ -209,7 +121,6 @@ app.message(/code|chat|write|function|InSecure|query|Python|debug|create|develop
     // console.timeEnd("secureCode");
 
     // await sleep(1500);
-
 
     // console.time("inSecureCode");
     // let insecureResponse = await inSecureCode();
---
+++ src/chat.js
@@ -0,0 +1,139 @@
+import { PromptTemplate } from "@langchain/core/prompts";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { ChatOllama } from "@langchain/ollama";
+import { z } from "zod";
+import { StructuredOutputParser } from "@langchain/core/output_parsers";
+
+
+export async function secureCode(userInput, systemPrompt) {
+  // Initialize the ChatOllama model
+  const llm = new ChatOllama({
+      model: "codegemma", // Specify the generative model
+      baseUrl: "http://localhost:11434", // Default Ollama API endpoint
+      temperature: 0.9, // Adjust for creativity
+      topK: 50,
+      // topP: 0.5,
+      // maxTokens: 32000, // Limit the response length
+      format: "json", // Optional: Use if you need structured JSON output
+      maxRetries: 10, // Number of retries for the model
+  });
+
+  console.log(`ChatOllama model initialized with model: ${llm.model}`);
+
+  // Define the schema for the structured output
+  const codeSchema = z.object({
+    code: z.string().describe("An example with code"),
+    explanation: z.string().describe("A paragraph length explanation of the code"),
+  });
+
+  // Create a structured output parser
+  const outputParser = StructuredOutputParser.fromZodSchema(codeSchema);
+
+  // Define the prompt template
+  const prompt = PromptTemplate.fromTemplate(systemPrompt);
+
+  // Combine the prompt with the structured output parser
+  const formattedPrompt = await prompt.format({
+    question: userInput, 
+    format_instructions: outputParser.getFormatInstructions(),
+  });
+
+  // Invoke the model with the formatted prompt
+  let response = "";
+
+  try {
+      response = await llm.invoke(formattedPrompt);
+  }
+  catch (error) {
+    console.error("Error invoking the model:", error);
+  }
+
+  console.log("Raw Response:",  JSON.parse(response.content))
+
+  return response;
+}
+
+
+export async function inSecureCode(userPrompt, systemPrompt) {
+
+    // Initialize the Ollama API and connect on TCP port 11434
+    const embeddings = new OllamaEmbeddings({
+        model: "mxbai-embed-large",
+        baseUrl: "http://127.0.0.1:11434",
+    });
+  
+    console.log(`\tOllamaEmbeddings initialized with model: ${embeddings.model}`);
+
+    // Load a vector store from embeddings
+    const vectorStore = new Chroma(embeddings, {
+    collectionName: "sql-injection",
+    persist: true,
+    persistDirectory: "../embeddings",
+    url: 'http://localhost:8000', // ChromaDB URL
+    });
+
+    // Assuming 'vectorStoreInstance' holds the populated Chroma instance from the previous step
+    if (!vectorStore) {
+    throw new Error("\tVector store instance is not available. Ingestion might have failed.");
+    }
+
+    const configuredRetriever = vectorStore.asRetriever({k:5});
+    console.log("\tRetriever created from Chroma vector store.");
+  
+    console.log(`\tVector Store loaded with: ${vectorStore.collectionName} ${vectorStore.embeddings.model}`);
+
+    const usercontext = await configuredRetriever.invoke(userPrompt);
+
+    const promptContext = {
+        pageContent: usercontext[0].pageContent,
+        source: usercontext[0].metadata.source
+    }
+
+    // Initialize the ChatOllama model
+    const llm = new ChatOllama({
+      model: "codellama:7b", // Specify the generative model
+      baseUrl: "http://localhost:11434", // Default Ollama API endpoint
+      temperature: 0.8, // Adjust for creativity
+      topK: 25,
+      topP: 0.9,
+      format: "json", // Optional: Use if you need structured JSON output
+      maxRetries: 10, // Number of retries for the model
+    });
+  
+    console.log(`\tChatOllama model initialized with model: ${llm.model}`);
+  
+    // Define the schema for the structured output
+    const codeSchema = z.object({
+        code: z.string().describe("A code example containing a vulnerability"),
+        explanation: z.string().describe("The explanation why the code is vulnerable"),
+        source: z.string().describe("Reference the source").optional(),
+    });
+  
+    // Create a structured output parser
+    const outputParser = StructuredOutputParser.fromZodSchema(codeSchema);
+  
+    // Define the prompt template
+    const prompt = PromptTemplate.fromTemplate(systemPrompt);
+
+    // Combine the prompt with the structured output parser
+    const formattedPrompt = await prompt.format({
+      prompt: userPrompt, 
+      //context: promptContext,
+      format_instructions: outputParser.getFormatInstructions(),
+    });
+  
+    // Invoke the model with the formatted prompt
+    let response = "";
+
+    try {
+       response = await llm.invoke(formattedPrompt);
+    }
+    catch (error) {
+      console.error("Error invoking the model:", error);
+    }
+  
+    console.log("\tRaw Response:",  JSON.parse(response.content))
+  
+    return response;
+  }
